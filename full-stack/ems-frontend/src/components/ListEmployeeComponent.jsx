0a4f216eaa41fd851b4e9252f8860cdd35589afa
NTDHaruto <dung11112003qng@gmail.com>
2024-03-05 03:35:10 +0700

    build: create react component to display employee data

---
+++ full-stack/ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const ListEmployeeComponent = () => {
+
+    const dummyData = [
+        {
+            "id":1,
+            "firstName":"Nguyễn",
+            "lastName":"Dũng",
+            "email":"dungnguyen@gmail.com"
+        },
+        {
+            "id":2,
+            "firstName":"Lê",
+            "lastName":"Tuân",
+            "email":"tuanle@gmail.com"
+        },
+        {
+            "id":3,
+            "firstName":"Cơ",
+            "lastName":"Nguyệt",
+            "email":"nguyetco@gmail.com"
+        }
+    ]
+
+  return (
+    <div className='container'>
+        <h2 className='text-center'>List of Employee</h2>
+        <table className='table table-striped table-bordered'>
+            <thead>
+                <tr>
+                    <th>Employee Id</th>
+                    <th>Employee First Name</th>
+                    <th>Employee Last Name</th>
+                    <th>Employee Email Id</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    dummyData.map(employee =>
+                        <tr key = {employee.id}>
+                            <td>{employee.id}</td>
+                            <td>{employee.firstName}</td>
+                            <td>{employee.lastName}</td>
+                            <td>{employee.email}</td>
+                        </tr>)
+                }
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default ListEmployeeComponent
