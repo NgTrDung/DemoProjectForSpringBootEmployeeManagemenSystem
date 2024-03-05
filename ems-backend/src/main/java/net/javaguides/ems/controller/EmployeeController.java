4fa91af2c6c3c6b35f291853312a8591351612e0
NTDHaruto <dung11112003qng@gmail.com>
2024-03-06 02:21:48 +0700

    build: connect react app to get all employees REST API

--- ems-backend/src/main/java/net/javaguides/ems/controller/EmployeeController.java
+++ ems-backend/src/main/java/net/javaguides/ems/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import lombok.AllArgsConstructor;
 import net.javaguides.ems.dto.EmployeeDTO;
 import net.javaguides.ems.service.EmployeeService;
 
+@CrossOrigin("*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
--- full-stack/ems-frontend/vite.config.js
+++ full-stack/ems-frontend/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
 
   server:{
-    port:8080
+    port:8081
   }
 })
