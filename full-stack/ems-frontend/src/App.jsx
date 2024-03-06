60fdde6c827a6228bb27af1e441ab5bea28d0cab
NTDHaruto <dung11112003qng@gmail.com>
2024-03-07 02:30:27 +0700

    build: create react EmployeeComponent & add route

--- full-stack/ems-frontend/src/App.jsx
+++ full-stack/ems-frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
+import EmployeesComponent from './components/EmployeesComponent'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponents from './components/HeaderComponents'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
@@ -10,13 +11,15 @@ function App() {
     <>
       <BrowserRouter>
         <HeaderComponents />
-          <Routes>
-            {/* // http://localhost:8081 */}
-            <Route path = '/' element = {<ListEmployeeComponent />}></Route>
-            {/* // http://localhost:8081/employees */}
-            <Route path = '/employees' element = {<ListEmployeeComponent />}></Route>
-          </Routes>
-        <FooterComponent/>
+        <Routes>
+          {/* // http://localhost:8081 */}
+          <Route path='/' element={<ListEmployeeComponent />}></Route>
+          {/* // http://localhost:8081/employees */}
+          <Route path='/employees' element={<ListEmployeeComponent />}></Route>
+          {/* // http://localhost:8081/add-employee */}
+          <Route path='/add-employee' element={<EmployeesComponent />}></Route>
+        </Routes>
+        <FooterComponent />
       </BrowserRouter>
     </>
   )
--- full-stack/ems-frontend/src/components/ListEmployeeComponent.jsx
+++ full-stack/ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -1,9 +1,11 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { listEmployees } from '../services/EmployeeService'
+import {useNavigate} from 'react-router-dom'
 
 const ListEmployeeComponent = () => {
 
     const [employees, setEmployees] = useState([])
+    const navigator = useNavigate();
 
     useEffect(() => {
         listEmployees().then((response) => {
@@ -13,32 +15,37 @@ const ListEmployeeComponent = () => {
         })
     }, [])
 
-  return (
-    <div className='container'>
-        <h2 className='text-center'>List of Employee</h2>
-        <table className='table table-striped table-bordered'>
-            <thead>
-                <tr>
-                    <th>Employee Id</th>
-                    <th>Employee First Name</th>
-                    <th>Employee Last Name</th>
-                    <th>Employee Email Id</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    employees.map(employee =>
-                        <tr key = {employee.id}>
-                            <td>{employee.id}</td>
-                            <td>{employee.firstName}</td>
-                            <td>{employee.lastName}</td>
-                            <td>{employee.email}</td>
-                        </tr>)
-                }
-            </tbody>
-        </table>
-    </div>
-  )
+    function addNewEmmployee(){
+        navigator('/add-employee')
+    }
+
+    return (
+        <div className='container'>
+            <h2 className='text-center'>List of Employee</h2>
+            <button className='btn btn-primary mb-2' onClick={addNewEmmployee}>Add Employee</button>
+            <table className='table table-striped table-bordered'>
+                <thead>
+                    <tr>
+                        <th>Employee Id</th>
+                        <th>Employee First Name</th>
+                        <th>Employee Last Name</th>
+                        <th>Employee Email Id</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        employees.map(employee =>
+                            <tr key={employee.id}>
+                                <td>{employee.id}</td>
+                                <td>{employee.firstName}</td>
+                                <td>{employee.lastName}</td>
+                                <td>{employee.email}</td>
+                            </tr>)
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default ListEmployeeComponent
